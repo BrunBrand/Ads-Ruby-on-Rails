77d9ac85ef1bf6d2134a9860a939cdbb6dd69567
LordFarron <brandon.brunno@gmail.com>
2020-10-15 19:31:11 -0300

    Add message when user inputs wrong credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,14 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
         user.authenticate(params[:user][:password]) #bcrypt method to authenticate
         reset_session
         session[:user_id] = user.id
         redirect_to root_path, notice: translate(".welcome", name: user.name)
-
+        else
+            redirect_to new_session_path, alert: translate(".invalid_credentials")
+        end
         
     end
 
--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -2,21 +2,38 @@ require "application_system_test_case"
 require 'factory_bot_rails'
 
 class LoginsTest < ApplicationSystemTestCase
-    
+  
+  def setup
+    @user = FactoryBot.create(:user)
+  end
+
   test "login com credencial valida" do 
-    user = FactoryBot.create(:user)
+   
 
     visit root_path
     click_link "Entrar"
 
-    fill_in "E-mail", with: user.email
+    fill_in "E-mail", with: @user.email
     fill_in "Senha", with: "password"
 
     click_button "Entrar"
 
     assert_text "Bem vindo Bruno!"
-
-
-    
   end
+
+  test "login com credencial invalida" do
+    
+    visit root_path
+    click_link "Entrar"
+
+    fill_in "E-mail", with: @user.email
+    fill_in "Senha", with: "invalidpassword"
+
+    click_button "Entrar"
+
+    assert_equal new_session_path, current_path
+    assert_text "E-mail ou senha invalidos"
+
+  end
+
 end

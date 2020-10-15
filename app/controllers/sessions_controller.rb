dd8ed5e2865906a355dffc9c3b857d1766904407
lordfarron <brandon.bruno@gmail.com>
2020-10-15 14:39:46 -0300

    Fix Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,13 @@ class SessionsController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        user = User.find_by(email: params[:user][:email])
+        user.authenticate(params[:user][:password]) #bcrypt method to authenticate
+        reset_session
+        session[:user_id] = user.id
+        redirect_to root_path, notice: translate(".welcome", name: user.name)
+    end
+
 end
--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -7,12 +7,12 @@ class LoginsTest < ApplicationSystemTestCase
     user = FactoryBot.create(:user)
 
     visit root_path
-    click_on "Entrar"
+    click_link "Entrar"
 
     fill_in "E-mail", with: user.email
     fill_in "Senha", with: "password"
 
-    #click_on "Entrar"
+    click_button "Entrar"
 
     assert_text "Bem vindo Bruno!"
 

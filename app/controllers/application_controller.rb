0b183413733fcc1c6f9f4f30b6f850b228d62652
LordFarron <brandon.brunno@gmail.com>
2020-10-16 00:10:59 -0300

    Add ads funcionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
+
+    def require_logged_user
+        redirect_to new_session_path, alert: "Você precisa estar logado" unless user_signed_in?
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
     def show
+        @ads = Ad.all
+
     end
 end
